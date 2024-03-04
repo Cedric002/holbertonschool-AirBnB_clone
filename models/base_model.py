559811180796ccce0dbfc9a9698ec196c73511e8
Spacefox95 <nathan.raynal@gmail.com>
2024-03-04 12:37:03 +0100

    Final_baseModel_task_1

--- models/base_model.py
+++ models/base_model.py
@@ -6,25 +6,39 @@ import json
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-	"""
-	The base class of the project
-	"""
+    """
+    The base class of the project
+    """
 
-	def __init__(self):
-		"""
-		Constructor for BaseModel
-		"""
-		self.id = str(uuid.uuid4)
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
-	
-	def save(self):
-		pass
+    def __init__(self):
+        """
+        Constructor for BaseModel
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
+    def save(self):
+        """
+        Update the updated_at instance with current datetime
+        """
+        self.updated_at = datetime.now()
 
-	def to_dict(self):
-		pass
+    def to_dict(self):
+        """
+        Return dict representation of the instance
+        """
+        object_dict = self.__dict__.copy()
+        object_dict['__class__'] = self.__class__.__name__
+        object_dict['created_at'] = self.created_at.isoformat()
+        object_dict['updated_at'] = self.updated_at.isoformat()
+        return object_dict
 
-	def __str__(self):
-		return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+    def __str__(self):
+        """
+        String representation of the object
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, str(self.id), self.__dict__)
