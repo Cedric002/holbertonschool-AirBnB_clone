6f9dc13742bb888d7b7eade23e98286a93524513
root <root@LAPTOP-PN3919K3>
2024-03-04 14:38:33 +0100

    add update base_model.py (task 4)

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,21 @@ class BaseModel:
 	The base class of the project
 	"""
 
-	def __init__(self, id, created_at, updated_at):
-		self.id = id
-		self.created_at = created_at
-		self.updated_at = updated_at
+	def __init__(self, *args, **kwargs):
+        if kwargs:
+            for attr_name, attr_value in kwargs.items():
+                if attr_name != "__class__":
+                    setattr(self, attr_name, attr_value)
+
+            # Convert created_at and updated_at strings to datetime objects
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            # Create id and created_at attributes
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 	
 	def save(self):
 		pass
