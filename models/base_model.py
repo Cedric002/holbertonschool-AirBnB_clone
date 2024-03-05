80389f12a19d59cdefb01a72288fce9e60c0b170
Spacefox95 <nathan.raynal@gmail.com>
2024-03-05 11:26:38 +0100

    Link_FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ Base model for the Airbnb console
 import json
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -16,24 +17,26 @@ class BaseModel:
         """
         Constructor for BaseModel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
                 elif key in ['created_at', 'updated_at']:
-                    # Convert string to datetime object
                     setattr(self, key, datetime.fromisoformat(value))
                 else:
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """
         Update the updated_at instance with current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,44 +4,46 @@ New class to serialize instances to a JSON
 """
 import json
 
-class FileStorage:
-	"""
-	File storage class
-	"""
-	__file_path = "file.json"
-	__objects = {}
 
-	def all(self):
-		"""
-		Returns the dictionnary __objects
-		"""
-		return self.__objects
-	
-	def new(self, obj):
-		"""
-		Sets in __object the obj with a special key
-		"""
-		key = "{}.{}".format(obj.__class__.__name__, obj.id)
-		self.__objects[key] = obj
-		
-	def save(self):
-		"""
-		Serializes __objects to the Json file
-		"""
-		obj_dict = {k: v.to_dict() for k, v in self.__objects.items()}
-		with open(self.__file_path, 'w') as file:
-			json.dump(obj_dict, file)
-		
-	def reload(self):
-		"""
-		Deserializes Json file to __objects
-		"""
-		try:
-			with open(self.__file_path, 'r') as file:
-				obj_dict = json.load(file)
-				for key, value in obj_dict.items():
-					class_name, obj_id = key.split('.')
-					obj = eval(class_name)(**value)
-					self.__objects[key] = obj
-		except FileNotFoundError:
-			pass
+class FileStorage:
+    """
+    File storage class
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionnary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __object the obj with a special key
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the Json file
+        """
+        obj_dict = {k: v.to_dict() for k, v in self.__objects.items()}
+        with open(self.__file_path, 'w') as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """
+        Deserializes Json file to __objects
+        """
+        try:
+            with open(self.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+                from models.base_model import BaseModel
+                for key, value in obj_dict.items():
+                    class_name, obj_id = key.split('.')
+                    obj = eval(class_name)(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
