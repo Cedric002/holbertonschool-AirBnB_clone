074636da1117d427cbc48c424da0a3fc5b7793a5
Spacefox95 <nathan.raynal@gmail.com>
2024-03-05 09:25:02 +0100

    Adding_kwarg_set_attr

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,18 @@ class BaseModel:
     The base class of the project
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Constructor for BaseModel
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if not __class__:
+                    setattr(self, key, value)
+
 
     def save(self):
         """
