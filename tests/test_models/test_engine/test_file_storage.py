2b2a86b3fb1e64939a604711e822b793bdaf2a72
Spacefox95 <nathan.raynal@gmail.com>
2024-03-05 11:59:50 +0100

    More_unittest

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -17,7 +17,25 @@ class TestFileStorage(unittest.TestCase):
 	
 	def test_object(self):
 		file_object = FileStorage()
-		self.assertIsInstance(file_object, FileStorage)
+		self.assertIsInstance(file_object._FileStorage__objects, dict)
+	
+	def test_all(self):
+		file_storage = FileStorage()
+		all_objects = file_storage.all()
+		self.assertIsInstance(all_objects, dict)
+
+	def test_new(self):
+		file_storage = FileStorage()
+		base_model = BaseModel()
+		file_storage.new(base_model)
+		obj_key = "BaseModel." + base_model.id
+		self.assertIn(obj_key, file_storage._FileStorage__objects)
+
+	def test_save(self):
+		pass
+
+	def test_reload(self):
+		pass
 
 if __name__ == "__main__":
     unittest.main()
