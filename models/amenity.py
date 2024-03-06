5e40e2b74252591d029e416be4b55100a4afbf48
Spacefox95 <nathan.raynal@gmail.com>
2024-03-06 16:03:38 +0100

    New_classes

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+New Class Amenity
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    New class Amenity that inherits from BaseModel
+    """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+New Class State
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    New class State that inherits from BaseModel
+    """
+    name = ""
